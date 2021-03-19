59a1835c6ce557c85029ebeced20e2eb1ee4aa89
edwar <you@example.com>
2021-03-18 22:55:08 -0500

    admitiendo mayusculas y minusculas

--- parcial.py
+++ parcial.py
@@ -1,4 +1,4 @@
-parrafo = 'La educación en valores es fundamental para cualquier persona,pues lo que aprendemos en los primeros  infruye en el prisma con el vemos el mundo'
+parrafo = 'La educación EN valores es fundamental para cualquier persona,pues lo que aprendemos en los primeros  infruye en el prisma con el vemos el mundo'
 
 
 
@@ -8,3 +8,14 @@ frecuenciapalab = [listapalabras.count(w) for w in listapalabras]
 
 print("parrafo\n" + parrafo +"\n")
 print("contador\n" + str(list(zip(listapalabras, frecuenciapalab))))
+
+def contar(parrafo):
+    contar = {'minusculas':0, 'mayusculas': 0}
+
+    for listapalabras in parrafo:
+        if listapalabras.isupper():
+            contar ['mayusculas'] += 1
+        elif listapalabras.islower():
+            contar ['minusculas'] += 1
+    return contar
+    
